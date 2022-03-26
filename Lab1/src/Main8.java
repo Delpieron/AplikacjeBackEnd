07061d41ec1d3304fe4c329dc3511e79c14f4474
Krzysztof Wroblewski <krzysztof.wroblewski@microsoft.wsei.edu.pl>
2022-03-26 14:25:15 +0100

    init lab2

---
+++ Lab1/src/Main8.java
@@ -0,0 +1,12 @@
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//public class Main8 {
+//    public static void main(String[] args) {
+//        String userJson = "{\"name\":\"John\",\"age\":21}";              // {"name":"John","age":21}
+//        User userObject = objectMapper.readValue(userJson, User.class);
+//
+//        System.out.println(userObject.getName());                        // John
+//        System.out.println(userObject.getAge());
+//    }
+//}
